d63914e0bed3142683264b899eba51ecb96dec72
Joe Froula <jmfroula@gmail.com>
2023-03-14 22:14:59 -0700

    initial

---
+++ components/Calendar.jsx
@@ -0,0 +1,87 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import listPlugin from '@fullcalendar/list'
+import interactionPlugin from "@fullcalendar/interaction"
+import { useState, useEffect, useRef } from 'react'
+
+
+export default function Calendar() {
+
+  const calendarRef = useRef(null);
+  const [activeView, setActiveView] = useState("dayGridMonth")
+  const [width, setWidth] = useState(null)  
+
+  useEffect(() => {
+
+    if (typeof window !== "undefined") { 
+
+      const handleResize = () => {
+        setWidth(window.innerWidth)
+        console.log(width)
+  
+        if (width < 900) {setActiveView("listMonth")} else {setActiveView("dayGridMonth")}
+      }
+  
+      window.addEventListener('resize', handleResize) 
+      
+      
+    }
+
+    console.log("View Changed", activeView);
+    const { current: calendarDom } = calendarRef;
+    const API = calendarDom ? calendarDom.getApi() : null;
+    API && API.changeView(activeView);
+  }, [activeView]);
+
+  useEffect(() => {
+
+    if (typeof window !== "undefined") { 
+      console.log(window)
+      if (window.innerWidth > 900) {
+        setActiveView("dayGridMonth")
+      } else { setActiveView("listWeek")}
+
+   }}, []);
+  
+
+    const events = [
+
+      { title: 'Meeting', start: '2023-03-09T22:54:52.289Z', description: 'Another meeting!' }, 
+      { title: 'Meeting', start: '2023-03-10T22:30:52.289Z', description: 'Another meeting!' }
+    ]
+
+    
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          setActiveView((previous) =>
+            previous === "dayGridMonth" ? "listMonth" : "dayGridMonth"
+          )
+        }
+      >
+        Toggle View
+      </button>
+      <h1>Demo App</h1>
+      <FullCalendar
+        plugins={[dayGridPlugin, listPlugin, interactionPlugin]}
+        initialView={activeView}
+        ref={calendarRef}
+        weekends={false}
+        events={events}
+        eventContent={renderEventContent}
+      />
+      
+    </div>)
+  
+}
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';  
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) { 
+  return <Component {...pageProps} /> 
 }
 
 export default MyApp
